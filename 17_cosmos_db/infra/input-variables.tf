f40029ae085277b07495b16efd1ceeb3ee1726dd
kokchun <kokchungiang@gmail.com>
2026-08-13 11:35:06 +0200

    refactor infra code

--- 17_cosmos_db/infra/input-variables.tf
+++ 17_cosmos_db/infra/input-variables.tf
@@ -9,18 +9,4 @@ variable "location" {
   default     = "swedencentral"
 }
 
-# sql-like syntax
-resource "azurerm_cosmosdb_sql_database" "db" {
-  name                = "FilmReviewDB"
-  resource_group_name = azurerm_resource_group.rg.name
-  account_name        = azurerm_cosmosdb_account.acc.name
-}
 
-resource "azurerm_cosmosdb_sql_container" "films_container" {
-  name                  = "Films"
-  resource_group_name   = azurerm_resource_group.rg.name
-  account_name          = azurerm_cosmosdb_account.acc.name
-  database_name         = azurerm_cosmosdb_sql_database.db.name
-  partition_key_paths   = ["/year"]
-  partition_key_version = 2
-}
